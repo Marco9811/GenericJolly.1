985792258fad36f28464d5d4fa402653ff05a9e3
Marco Abbondanza <107813503+Marco9811@users.noreply.github.com>
2023-01-26 21:59:44 +0100

    Initial commit

---
+++ src/it/Develhope/Student.java
@@ -0,0 +1,20 @@
+package it.Develhope;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Student {
+    String studentName;
+    String studentSurname;
+    double marksAverage;
+    public void Student(){
+        System.out.println(studentName + " " + studentSurname + " "+ marksAverage);
+        this.studentName = "marco";
+        this.studentSurname = "Abbondanza";
+        this.marksAverage = 8.5;
+    }
+     static void getAverageMark() {
+         int listOfMarks;
+     }
+     List <? number> = new ArrayList()<>
+}
